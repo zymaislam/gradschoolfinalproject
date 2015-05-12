d5aa73f7dab5a5751d8f3e8e49ecaa9ad6406bec
zymaislam <zyma.islam90@gmail.com>
2015-05-12 15:06:51 -0400

    updated final scripts

--- Cost_Zyma/scripts.js
+++ Cost_Zyma/scripts.js
@@ -6,95 +6,111 @@ google.load('visualization', '1.0', {
 // Set a callback to run when the Google Visualization API is loaded.
 google.setOnLoadCallback(drawChart);
 function drawChart() {
-	var data = new google.visualization.DataTable();
-	data.addColumn('string', 'Year');
-	data.addColumn('number', 'Cost families with kids');
-	data.addColumn({
-		type : 'string',
-		role : 'annotation'
-	});
-	data.addColumn('number', '90% of Cost families with kids');
-	data.addColumn({
-		type : 'string',
-		role : 'annotation'
-	});
-	
-	data.addColumn('number', 'Cost families with adults');
-	data.addColumn({
-		type : 'string',
-		role : 'annotation'
-	});
-	data.addColumn('number', '80% of Cost families with adults');
-	data.addColumn({
-		type : 'string',
-		role : 'annotation'
-	});
-	data.addColumn('number', 'Cost single adults');
-	data.addColumn({
-		type : 'string',
-		role : 'annotation'
-	});
-		data.addColumn('number', '80% of Cost single adults');
-	data.addColumn({
-		type : 'string',
-		role : 'annotation'
-		});
-	
-	
-	
-	
- 	data.addRows(dhsData);
 
-	var options = {
-		'title' : 'How Much Pizza I Ate Last Night',
-		'width' : 1200,
-		'height' : 700,
-		/*intervals : {
-			'lineWidth' : 4,
-			'barWidth' : 0.5,
-			'color' : '#a46eb7'
-		},*/
-chartArea: 1100,
-		colors : ['#bad80a', '#e5f871','#636864','#C6D1C8','#a46eb7','#ddc9e4',],
-		focusTarget: 'category',
-    // Use an HTML tooltip.
-    tooltip: { isHtml: true,  legend: 'none' },
-     
-		dataOpacity : 0.6,
+	
+	
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Category');
+	data.addColumn('number', 'How much the DHS spent from 2011 till now                              ');
+	data.addColumn({
+		type : 'string',
+		role : 'annotation'
+	}); 
+	
+data.addColumn({type: 'string', role: 'tooltip'});
+data.addColumn({
+		type : 'string',
+		role : 'style'
+	}); 
+	
+	data.addColumn('number', 'How much it would have spent after Project Reconnect');
+	data.addColumn({
+		type : 'string',
+		role : 'annotation'
+	}); 
+	data.addColumn({
+		type : 'string',
+		role : 'style'
+	}); 
+
+data.addColumn({type: 'string', role: 'tooltip'});
+
+	data.addRows(dhsData);
+		var options = {
+			chartArea: {width: '80%', height: '60%'},
+			bar: {groupWidth: "100%"},
+			annotations: {
+				alwaysOutside:false},
+				annotations: {
+      textStyle: {fontName: 'trebuchet MS'}},
+   backgroundColor:'#FAFAFA',
+		width : 1000,
+		height : 600,
+		colors : ['#7abd03',  '#590059'],
+		
+			
+	
+	
+	
+	
+	
+		legend: {position: 'top',alignment: 'end', textStyle: {color: '#283e7a', fontSize: 16, fontName: 'trebuchet MS'}
+			 },
+	vAxis: {
+		
+				
+	
+	 ticks: [ 0, 
+{v:400000000, f:'$0.5 billion'}, 
+{v:1000000000, f:'$1 billion'},
+{v:2000000000, f:'$2 billion'},
+{v:3000000000, f:'$3 billion'},
+{v:4000000000, f:'$4 billion'},
+{v:5000000000, f:'$5 billion'}, ] 
+
+}, 
+		
+		
+	 
+		// Use an HTML tooltip.
+		tooltip : {
+			isHtml : true,
+			showColorCode:true,
+			
+	textStyle:{
+  fontName: 'trebuchet MS',
+
+ 
+  bold: true,
+  italic: false }
+			
+		},
+
+		dataOpacity : 0.5,
 		annotations : {
-			alwaysOutside: true}
-			/*highContrast : true,
+			alwaysOutside : true,
+
 			textStyle : {
 				fontName : 'arial',
-				color : 'black',
-				fontSize : 14,
+				fontSize : 18,
 				bold : false,
 				italic : false,
-				// The color of the text.
-				auraColor : 'transparent', // The color of the text outline.
+
 				opacity : 0.8
-			}, // The transparency of the text.
-			boxStyle : {
-				//stroke : '#888', // Color of the box outline.
-				//strokeWidth : 1, // Thickness of the box outline.
-				rx : 10, // x-radius of the corner curvature.
-				ry : 10, // y-radius of the corner curvature.
-				gradient : {// Attributes for linear gradient fill.
-					color1 : 'white', // Start color for gradient.
-					color2 : 'white', // Finish color for gradient.
-					x1 : '0%',
-					y1 : '0%', // Where on the boundary to start and end the
-					x2 : '100%',
-					y2 : '100%', // color1/color2 gradient, relative to the
-					// upper left corner of the boundary.
-					useObjectBoundingBoxUnits : false // If true, the boundary for x1, y1,
-					// x2, and y2 is the box. If false,
-					// it's the entire chart.
-				}
 			}
+		},
+		hAxis : {
+			textStyle : {
+				color : '#95a3ad',
+				fontName : 'arial',
+				fontSize : 26,
+				bold : false,
+				italic : false
+			}
+
 		}
-	*/
-};
+
+	};
 	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
 	chart.draw(data, options);
 
